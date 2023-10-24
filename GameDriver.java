366472216709056fe69bfd441877d521fb4dbfb2
Doom8675309 <142505785+Doom8675309@users.noreply.github.com>
2023-10-24 12:18:36 -0400

    Update GameDriver.java
    Fixing the code to be under col 80

--- GameDriver.java
+++ GameDriver.java
@@ -7,29 +7,34 @@ import java.util.*;
 
 public class GameDriver {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner;
+        int choice;
+        scanner= new Scanner(System.in);
 
         System.out.println("Welcome to the Game Center!");
         
         boolean playAgain;
         do {
+        // Asks what game would like to be played
             System.out.println("Select a game to play:");
             System.out.println("1. Connect Four");
             System.out.println("2. Another Game (To Be Implemented)");
 
-            int choice = scanner.nextInt();
-
+            choice = scanner.nextInt();
+       // Choices for games go here
             switch (choice) {
                 case 1:
                     playConnectFour();
                     break;
                 case 2:
-                    System.out.println("Sorry, the second game is not implemented yet.");
+                    System.out.println("Sorry, the second game is not" +
+                        " implemented yet.");
                     break;
                 default:
-                    System.out.println("Invalid choice. Please select a valid game.");
+                    System.out.println("Invalid choice. Please select a" +
+                     " valid game.");
             }
-
+       // When game is done ask if you want to change to another game
             System.out.print("Do you want to play another game? (yes/no): ");
             String response = scanner.next().toLowerCase();
             playAgain = response.equals("yes");
@@ -37,19 +42,22 @@ public class GameDriver {
 
         System.out.println("Thanks for playing at the Game Center!");
     }
-
+       // Code for game 1 
     public static void playConnectFour() {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner;
+        String player1Name, player2Name;
+        scanner = new Scanner(System.in);
 
         System.out.println("Welcome to Connect Four!");
-
-        String player1Name = getPlayerName(scanner, "Player 1");
-        String player2Name = getPlayerName(scanner, "Player 2");
-
+       // Asks for player name in order to correctly show whos turn it is
+        player1Name = getPlayerName(scanner, "Player 1");
+        player2Name = getPlayerName(scanner, "Player 2");
+       // Running code to see if the player would like to play again
         boolean playAgain;
         do {
             ConnectFourGame.playConnectFour(player1Name, player2Name);
-            System.out.print("Do you want to play Connect Four again? (yes/no): ");
+            System.out.print("Do you want to play Connect Four again?" +
+            "(yes/no): ");
             String response = scanner.next().toLowerCase();
             playAgain = response.equals("yes");
         } while (playAgain);
